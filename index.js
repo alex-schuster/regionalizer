a7e32ddc40bba013964290a1bb5bbd968053fe72
Alexander Schuster <alexander@top11.at>
2020-12-09 18:30:18 +0100

    Refactors the project

--- index.js
+++ index.js
@@ -3,56 +3,107 @@
  * @author Alexander Schuster <alexander@top11.at>
  */
 
-import { showView } from './lib/viewManager';
-import { setDefinitions } from './lib/definitionReader';
+import createDefinitionReader from './lib/definitionReader';
+import createViewManager from './lib/viewManager';
 import { getAnchor } from './lib/utility';
 
-export { setLang, showView } from './lib/viewManager';
-
 /**
- * Initializes regionalizer.
+ * Creates and returns a regionalizer object.
+ *
+ * @param {Object} providedDefinitions The definitions to be used to manage the application.
+ * @return {Object} The regionalizer object.
  */
-export const init = (definitions) => {
-  // Pass the definitons object to the definionReader.
-  setDefinitions(definitions);
+const createRegionalizer = (providedDefinitions) => {
+  /**
+   * Holds the definitionReader instance.
+   *
+   * @type {Object}
+   */
+  const definitionReader = createDefinitionReader(providedDefinitions);
 
-  // Handle in-app links.
-  const body = document.getElementsByTagName('body')[0];
+  /**
+   * Holds the viewManager instance.
+   *
+   * @type {Object}
+   */
+  const viewManager = createViewManager(definitionReader);
 
-  body.addEventListener('click', (event) => {
-    // Get the "a" tag of a click event.
-    const anchor = getAnchor(event.target);
+  return {
+    /**
+     * Initializes regionalizer.
+     */
+    init() {
+      // Handle in-app links.
+      const body = document.querySelector('body');
 
-    // Return if the click was not on a link.
-    if (anchor === null) {
-      return;
+      body.addEventListener('click', (event) => {
+        // Get the "a" tag of a click event.
+        const anchor = getAnchor(event.target);
+
+        // Return if the click was not on a link.
+        if (anchor === null) {
+          return;
+        }
+
+        // Return if the link leads to another website.
+        if (anchor.origin !== window.location.origin) {
+          return;
+        }
+
+        // Return if the link should be opened in a new tab.
+        if (anchor.matches('a[target="_blank"]')) {
+          return;
+        }
+
+        // Fire the regionalizer.inAppNavigation event to allow for render data injection.
+        event.preventDefault();
+        const path = event.target.pathname;
+        const eventProps = { ...event };
+        eventProps.detail = { path };
+        const inAppLinkClickEvent = new CustomEvent('regionalizer.inAppNavigation', eventProps);
+        window.dispatchEvent(inAppLinkClickEvent);
+
+        // Update history.
+        if (anchor.href !== window.location.href) {
+          window.history.pushState({ pathVariable: event.target.pathname }, '', event.target.href);
+        }
+      }, true);
+
+      // Handle the event when the user clicks the back button of the browser.
+      window.addEventListener('popstate', (event) => {
+        if (!event.state) {
+          return;
+        }
+
+        // Fire the regionalizer.inAppNavigation event to allow for render data injection.
+        const path = event.state.pathVariable;
+        const eventProps = { ...event };
+        eventProps.detail = { path };
+        const inAppPopstate = new CustomEvent('regionalizer.inAppNavigation', eventProps);
+        window.dispatchEvent(inAppPopstate);
+      });
+    },
+
+    /**
+     * Initiates a view change.
+     *
+     * @param {string} path The path for the view to be loaded.
+     * @param {Object} renderData The data object to be used inside the templates.
+     * @param {Object} headData The data object to be used for rendering the "head" section.
+     * @param {string} langCode The short code describing the language of the content.
+     * @param {string} textDirection The text direction ("ltr" or "rtl").
+     */
+    changeView(path, renderData, headData, langCode, textDirection) {
+      return new Promise((resolve, reject) => {
+        try {
+          viewManager.insertView(path, renderData, headData, langCode, textDirection);
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
     }
-
-    // Return if the link leads to another website.
-    if (anchor.origin !== window.location.origin) {
-      return;
-    }
-
-    // Return if the link should be opened in a new tab.
-    if (anchor.matches('a[target="_blank"]')) {
-      return;
-    }
-
-    event.preventDefault();
-    showView(event.target.pathname, { head: { dummy: '' } });
-
-    // Update history.
-    if (anchor.href !== window.location.href) {
-      window.history.pushState({ pathVariable: event.target.pathname }, '', event.target.href);
-    }
-  }, true);
-
-  // Handle the event when the user clicks the back button of the browser.
-  window.addEventListener('popstate', (event) => {
-    if (!event.state) {
-      return;
-    }
-    const path = event.state.pathVariable;
-    showView(path, { head: { dummy: '' } });
-  });
+  };
 };
+
+export default createRegionalizer;
